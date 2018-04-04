c95df04ced1eae9b358da51147aa1ac2bff01588
tecnalia <tecnalia@FHP>
2018-04-04 15:30:30 +0200

    FlexOffer code completed

---
+++ usef-build/usef-workflow/usef-brp/src/main/java/energy/usef/brp/repository/dataModelFHP/MarketRealPtuRepository.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015-2016 USEF Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package energy.usef.brp.repository.dataModelFHP;
+
+import energy.usef.brp.model.dataModelFHP.MarketRealPtu;
+import energy.usef.core.repository.BaseRepository;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.TemporalType;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+
+/**
+ * @author TECNALIA
+ */
+/**
+ * MarketRealPtu Repository for BRP.
+ */
+@Stateless
+public class MarketRealPtuRepository extends BaseRepository<MarketRealPtu> {
+
+    /**
+     * Return the MarketRealPtu.
+     * 
+     * @param marketRealId
+     * @param ptuStartDateTime
+     * @param ptuEndDateTime
+     * @return MarketRealPtu entities
+     */
+    public MarketRealPtu get(long marketRealId, LocalDateTime ptuStartDateTime,
+            LocalDateTime ptuEndDateTime) {
+        StringBuilder queryString = new StringBuilder("SELECT ptu FROM MarketRealPtu ptu");
+        queryString.append(" WHERE ptu.marketRealId = :marketRealId");
+        queryString.append(" AND ptu.startDatetime <= :ptuStartDateTime");
+        queryString.append(" AND ptu.endDatetime >= :ptuEndDateTime");
+        List<MarketRealPtu> result = entityManager.createQuery(queryString.toString(), MarketRealPtu.class)
+                .setParameter("marketRealId", marketRealId)
+                .setParameter("ptuStartDateTime", ptuStartDateTime.toDateTime().toDate(), TemporalType.TIMESTAMP)
+                .setParameter("ptuEndDateTime", ptuEndDateTime.toDateTime().toDate(), TemporalType.TIMESTAMP)
+                .getResultList();
+        if(result == null || result.isEmpty())
+            return null;
+                
+        return result.get(0);
+    }
+}
---
+++ usef-build/usef-workflow/usef-brp/src/main/java/energy/usef/brp/repository/dataModelFHP/MarketRealRepository.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015-2016 USEF Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package energy.usef.brp.repository.dataModelFHP;
+
+import energy.usef.brp.model.dataModelFHP.MarketReal;
+import energy.usef.core.repository.BaseRepository;
+import energy.usef.core.util.DateTimeUtil;
+
+import javax.ejb.Stateless;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+
+/**
+ * @author TECNALIA
+ */
+/**
+ * MarketReal Repository for BRP.
+ */
+@Stateless
+public class MarketRealRepository extends BaseRepository<MarketReal> {
+
+    /**
+     * Initialize MarketReal table for a determined date.
+     * 
+     * @param startDate
+     * @param ptuDuration
+     * @param numberOfPtusPerDay
+     * @return MarketReal Id
+     */
+    public long InitializeTestValues(LocalDate startDate, Integer ptuDuration, int numberOfPtusPerDay) {
+        MarketReal marketReal = new MarketReal();
+        //TODO: Initialize MarketReal and MarketRealPtu tables with Test values
+        LocalDateTime now = DateTimeUtil.getCurrentDateTime();
+        marketReal.setDatetime(now.toDateTime().toDate());
+       
+        marketReal.setStartDate(startDate.toDateMidnight().toDate());
+        
+        persist(marketReal);
+        return marketReal.getId();
+    }
+}
